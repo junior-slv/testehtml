d497a2c74f3f229bdc68a9dadeab9c77e210d57f
junior-slv <dirceu.junior@edu.unifil.br>
2024-02-27 18:14:03 -0300

    dasdasd

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = 3000;
+
+const allowedOrigin = "http://localhost:8080";
+
+const corsOptions = {
+  origin: allowedOrigin,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+};
+
+app.use(cors(corsOptions));
+
+app.get("/api", (req, res) => {
+  console.log(req.body);
+  res.send("Hello, this is the /api route using GET!");
+});
+
+app.post("/api", (req, res) => {
+  console.log(req.body);
+  res.send("Hello, this is the /api route using POST!");
+});
+
+app.put("/api", (req, res) => {
+  console.log(req.body);
+  res.send("Hello, this is the /api route using PUT!");
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
